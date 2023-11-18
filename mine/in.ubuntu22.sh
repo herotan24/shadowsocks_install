2f0ee3a17492b6e76aa9e41fd4b936355fb01dc8
herotan24 <yuhua_tan24@sina.com>
2023-11-18 23:01:17 +0800

    for ubuntu22

--- mine/in.ubuntu22.sh
+++ mine/in.ubuntu22.sh
@@ -5,3 +5,9 @@
 
 vimdiff /usr/local/lib/python3.10/dist-packages/shadowsocks-3.0.0-py3.10.egg/shadowsocks/crypto/util.py util.py
 vimdiff /usr/local/lib/python3.10/dist-packages/shadowsocks-3.0.0-py3.10.egg/shadowsocks/lru_cache.py lru_cache.py
+
+#sudo /etc/init.d/shadowsocks-python restart
+#sudo /etc/init.d/shadowsocks-python status
+#sudo vim /etc/shadowsocks-python/config.json
+#vim /etc/shadowsocks-python/config.json
+#sudo /etc/init.d/shadowsocks-python start
